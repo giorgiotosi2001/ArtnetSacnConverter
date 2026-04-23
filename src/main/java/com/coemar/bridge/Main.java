daa46b92a5a54facc2da8826f5432b8ab45a529c
giorgiotosi2001 <148875771+giorgiotosi@users.noreply.github.com>
2026-04-23 08:51:19 +0200

    Messi i receiver separati

--- src/main/java/com/coemar/bridge/Main.java
+++ src/main/java/com/coemar/bridge/Main.java
@@ -4,8 +4,13 @@ import com.coemar.bridge.network.UdpReceiver;
 
 public class Main {
     public static void main(String[] args) {
+        // Lo posso fare ? Funziona ? Non è un casino ? Vediamo
         UdpReceiver receiver = new UdpReceiver();
-        //receiver.startArtNetReceiver();
-        receiver.startSacnReceiver();
+
+        Thread t1 = new Thread(receiver::startSacnReceiver);
+        t1.start();
+
+        Thread t2 = new Thread(receiver::startArtNetReceiver);
+        t2.start();
     }
 }
